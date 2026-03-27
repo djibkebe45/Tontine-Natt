e0281165d63b0a5587921ac4c8cbfd7053ef7c55
djibkebe45 <djibyk996@gmail.com>
2026-03-27 07:56:41 +0000

    Create app.config.js

---
+++ app.config.js
@@ -0,0 +1,17 @@
+export default {
+  expo: {
+    name: "Tontine Natt",
+    slug: "tontine-natt",
+    version: "1.0.0",
+    orientation: "portrait",
+    platforms: ["android", "ios"],
+    android: {
+      package: "com.kebeson.tontinenatt"
+    },
+    extra: {
+      eas: {
+        projectId: "ton-project-id"
+      }
+    }
+  }
+};
